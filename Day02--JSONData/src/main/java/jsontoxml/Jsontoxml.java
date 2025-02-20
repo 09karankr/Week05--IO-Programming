d7e154b8457de92b872ee5a6d62f3d0a7cfbdc53
Karan Kumar <officialkaran39@gmail.com>
2025-02-20 11:10:22 +0530

    [Karan Kumar]Add. Added the program for converting json to xml format

---
+++ Day02--JSONData/src/main/java/jsontoxml/Jsontoxml.java
@@ -0,0 +1,20 @@
+package jsontoxml;
+
+import org.json.JSONObject;
+import org.json.XML;
+public class Jsontoxml {
+    public static void main(String[] args) {
+        String jsonString = "{"
+                + "\"employee\": {"
+                + "   \"name\": \"John Doe\","
+                + "   \"age\": 30,"
+                + "   \"email\": \"john@example.com\","
+                + "   \"department\": \"IT\""
+                + "}"
+                + "}";
+
+        JSONObject json = new JSONObject(jsonString);
+        String xml = XML.toString(json, "root");
+        System.out.println(xml);
+    }
+}
