34deec4f12ba468c7c29a51eca31138f9cdb36c9
Karan Kumar <officialkaran39@gmail.com>
2025-02-20 11:02:50 +0530

    [Karan Kumar]Add. Added the program for creating json object

---
+++ Day02--JSONData/src/main/java/createjson/Main.java
@@ -0,0 +1,22 @@
+package createjson;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        JSONArray subjects = new JSONArray();
+        subjects.put("mathematics");
+        subjects.put("physics");
+        subjects.put("chemistry");
+
+        JSONObject user = new JSONObject();
+        user.put("name" ,"Alice");
+        user.put("age",12);
+        user.put("subjects",subjects);
+
+        System.out.println(user.toString(3));
+
+    }
+}
